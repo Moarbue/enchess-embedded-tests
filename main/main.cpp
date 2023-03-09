936ab08bd7c1e80ee4881bca79a209aee796fe4d
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-03-09 09:38:37 +0100

    Use setup() and loop()

--- main/main.cpp
+++ main/main.cpp
@@ -7,19 +7,18 @@
 #include "tmc2209_test.h"
 
 
-extern "C" void app_main()
+void setup()
 {
-    initArduino();
-    
     led_test_setup();
     el_mag_test_setup();
     tmc_test_setup();
     tmc2209_test_setup();
+}
 
-    while(1) {
-        led_test_run();
-        el_mag_test_run();
-        tmc_test_run();
-        tmc2209_test_run();
-    }
+void loop()
+{
+    led_test_run();
+    el_mag_test_run();
+    tmc_test_run();
+    tmc2209_test_run();
 }

5c4e0ce9ba9c129f01531f73e3222ae1b54d23c8
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-04-11 19:45:19 +0200

    Automatic new-line

--- main/util.h
+++ main/util.h
@@ -1,6 +1,6 @@
 #ifndef ENCHESS_UTIL_H
 #define ENCHESS_UTIL_H
 
-#define LOG_MSG(fmt, ...) (printf(fmt, ##__VA_ARGS__))
+#define LOG_MSG(fmt, ...) (printf(fmt "\r\n", ##__VA_ARGS__))
 
 #endif // ENCHESS_UTIL_H

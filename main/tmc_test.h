4f4bb5e8b832ea577e4b9d3e9b4e26067422cf67
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-04-17 16:29:36 +0200

    Read and print out result

--- main/tmc_test.h
+++ main/tmc_test.h
@@ -6,7 +6,7 @@
 
 #define is_bit_set(value, bit) (((value) & (1 << bit)) ? HIGH : LOW)
 
-#define TMC_INTERVAL 2000
+#define TMC_INTERVAL 1000
 
 static uint64_t tmc_last_queried;
 static uint8_t tmc_index = 0;
@@ -37,6 +37,8 @@ static inline void tmc_test_run(void)
         digitalWrite(ENCHESS_PIN_TMC_3, is_bit_set(tmc_index, 3));
         digitalWrite(ENCHESS_PIN_TMC_4, is_bit_set(tmc_index, 4));
         digitalWrite(ENCHESS_PIN_TMC_5, is_bit_set(tmc_index, 5));
+
+        LOG_MSG("Sensor %u = %s", tmc_index, digitalRead(ENCHESS_PIN_TMC_OUT) ? "EMPTY" : "OCCUPIED");
 
         tmc_index = tmc_index < 64 ? tmc_index + 1 : 0;
     }
